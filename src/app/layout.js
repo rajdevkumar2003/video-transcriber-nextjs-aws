eb876870e95021d7b9368caa44b8182adcccb3ff
Rajeev <rajeevkumarjbo2003@gmail.com>
2023-12-05 00:31:48 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-gradient-to-b from-blue-500 to-purple-300 h-full m-7"}>
+      <Header/>
+      {children}
+      <Footer/>
+      </body>
     </html>
   )
 }
